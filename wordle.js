6ca3882e5db202010b7209780dd95bfffcb75272
sigmalorah25 <lmbgarcia22@src.pshs.edu.ph>
2025-03-04 11:11:08 +0800

    Update wordle.js

--- wordle.js
+++ wordle.js
@@ -1,142 +1,146 @@
 var wordleWords = [ 'waltz', 'sigma','brain', 'media', 'drama','child', 'quest', 'eight', 'scout', 'world','shove', 'stone', 'light', 'coral', 'chair',
-                    'rocks', 'stand', 'video', 'mango', 'solid', 'spark', 'moles', 'heavy', 'plane', 'nymph', 'quick', 'fjord', 'block', 'blast', 
-                    'knave', 'crazy', 'frank', 'ocean', 'share', 'third', 'money', 'junky', 'crash', 'toast', 'cable', 'solve' ];
+    'rocks', 'stand', 'video', 'mango', 'solid', 'spark', 'moles', 'heavy', 'plane', 'nymph', 'quick', 'fjord', 'block', 'blast', 
+    'knave', 'crazy', 'frank', 'ocean', 'share', 'third', 'money', 'junky', 'crash', 'frame', 'cable', 'solve' ];
 var wordleWord;
 
 var health = 5;
 var score = 0;
 
 function setupGame() {
-    generateWord();
-    console.log(wordleWord)
+generateWord();
+console.log(wordleWord)
 }
 
 function generateWord() {
-    let randomIndex = Math.trunc(Math.random() * wordleWords.length);
-    wordleWord = wordleWords[randomIndex];
+let randomIndex = Math.trunc(Math.random() * wordleWords.length);
+wordleWord = wordleWords[randomIndex];
 }
 function checkWord(){
 
-    let guess = prompt('Enter a five-letter guess').toLowerCase();
+let guess = prompt('Enter a five-letter guess').toLowerCase();
 
-    if (  (guess.length !== 5)) {
-        alert('Enter a 5-letter word.');
-        return;
-    }
-    else {
+if (  (guess.trim() == "" ) || (guess.length !== 5)) {
+alert('Enter a 5-letter word.');
+return;
+}
+else {
 
-    }
+}
 
-   var rowIndex = 5 - health;
-   if (rowIndex < 0) return;
+var rowIndex = 5 - health;
+if (rowIndex < 0) return;
 
-    var cell0 = document.getElementById(`r-${rowIndex}-0`);
-    var cell1 = document.getElementById(`r-${rowIndex}-1`);
-    var cell2 = document.getElementById(`r-${rowIndex}-2`);
-    var cell3 = document.getElementById(`r-${rowIndex}-3`);
-    var cell4 = document.getElementById(`r-${rowIndex}-4`);
+var cell0 = document.getElementById(`r-${rowIndex}-0`);
+var cell1 = document.getElementById(`r-${rowIndex}-1`);
+var cell2 = document.getElementById(`r-${rowIndex}-2`);
+var cell3 = document.getElementById(`r-${rowIndex}-3`);
+var cell4 = document.getElementById(`r-${rowIndex}-4`);   
 
-    function letterChecker(letter) {
-        if (wordleWord[0] === letter) {
-            return true;}
-        if (wordleWord[1] === letter) {
-            return true;}
-        if (wordleWord[2] === letter) {
-            return true;}
-        if (wordleWord[3] === letter) {
-            return true;}
-        if (wordleWord[4] === letter) {
-            return true;}
-       else                           {  
-        return false;   }
-    }
+function letterChecker(letter) {
+if (wordleWord[0] === letter) {
+return true;}
+if (wordleWord[1] === letter) {
+return true;}
+if (wordleWord[2] === letter) {
+return true;}
+if (wordleWord[3] === letter) {
+return true;}
+if (wordleWord[4] === letter) {
+return true;}
+else                           {  
+return false;   }
+}
 
-    if (guess.length == 5) {
+score = 0;
 
-        if (guess[0] == wordleWord[0]) {
-            cell0.classList.add('r-0', 'correct');
-            score++;
-        }
+if (guess.length == 5) {
 
-        else if (letterChecker(guess[0])){
-            cell0.classList.add('r-0', 'present');
-        }
+if (guess[0] == wordleWord[0]) {
+cell0.style.backgroundColor = 'green';
+score++;
+}
 
-        else {
-            cell0.classList.add('r-0', 'absent');
-        }
+else if (letterChecker(guess[0])){
+cell0.style.backgroundColor = 'yellow';
+}
 
-        cell0.innerText = guess [0];
+else {
+cell0.style.backgroundColor = 'gray';
+}
 
-        if (guess[1] == wordleWord[1]) {
-            cell1.classList.add('r-1', 'correct');
-            score++;
-        }
+cell0.innerHTML = guess [0];
 
-        else if (letterChecker(guess[1])) {
-            cell1.classList.add('r-1', 'present');
-        }
+if (guess[1] == wordleWord[1]) {
+cell1.style.backgroundColor = 'green';
+score++;
+}
 
-        else {
-            cell1.classList.add('r-1', 'absent');
-        }
+else if (letterChecker(guess[1])) {
+cell1.style.backgroundColor = 'yellow';
+}
 
-        cell1.innerText = guess [1];
+else {
+cell1.style.backgroundColor = 'gray';
+}
 
-        if (guess[2] == wordleWord[2]) {
-            cell2.classList.add('r-2', 'correct');
-            score++;
-        }
+cell1.innerHTML = guess [1];
 
-        else if (letterChecker(guess[2])) {
-            cell2.classList.add('r-2', 'present');
-        }
+if (guess[2] == wordleWord[2]) {
+cell2.style.backgroundColor = 'green';
+score++;
+}
 
-        else {
-            cell2.classList.add('r-2', 'absent');
-        }
+else if (letterChecker(guess[2])) {
+cell2.style.backgroundColor = 'yellow';
+}
 
-        cell2.innerText = guess [2];
+else {
+cell2.style.backgroundColor = 'gray';
+}
 
-        if (guess[3] == wordleWord[3]) {
-            cell3.classList.add('r-3', 'correct');
-            score++;
-        }
+cell2.innerHTML = guess [2];
 
-        else if (letterChecker(guess[0])) {
-            cell3.classList.add('r-3', 'present');
-        }
+if (guess[3] == wordleWord[3]) {
+cell3.style.backgroundColor = 'green';
+score++;
+}
 
-        else {
-            cell3.classList.add('r-3', 'absent');
-        }
+else if (letterChecker(guess[3])) {
+cell3.style.backgroundColor = 'yellow';
+}
 
-        cell3.innerText = guess [3];
+else {
+cell3.style.backgroundColor = 'gray';
+}
 
-        if (guess[4] == wordleWord[4]) {
-            cell4.classList.add('r-4', 'correct');
-            score++;
-        }
+cell3.innerHTML = guess [3];
 
-        else if (letterChecker(guess[4])) {
-            cell4.classList.add('r-4', 'present');
-        }
+if (guess[4] == wordleWord[4]) {
+cell4.style.backgroundColor = 'green';
+score++;
+}
 
-        else {
-            cell4.classList.add('r-4', 'absent');
-        }
+else if (letterChecker(guess[4])) {
+cell4.style.backgroundColor = 'yellow';
+}
 
-        cell4.innerText = guess [4];
+else {
+cell4.style.backgroundColor = 'gray';
+}
 
-        if (score==5) {
-            alert('You won! To play again, reload the page.')
-        }
-        else{
-            health--;
-            if (health === 0){
-            alert('You lost! To play again, reload the page.')
-        }
-    }
+cell4.innerHTML = guess [4];
 
-    }
+health--;
+
+if (guess == wordleWord) {
+alert('You won! To play again, reload the page.')
+health = -1;
+return;
+}
+
+if (health === 0){
+alert(`You lost! The word was ${wordleWord.toUpperCase()} !!! ^__^ To play again, reload the page.`)
+}
+}
+
 }
